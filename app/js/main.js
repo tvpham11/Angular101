52f74db8f5df2b3520b693642f94cb3ab54bad74
Vi Pham <tvpham11@gmail.com>
2015-06-24 09:12:44 -0400

    finished to-do list

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,38 @@
-console.log('This is cool!');
+;(function () {
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+  'use strict';
+
+  angular.module('CoolModule', [])
+
+  .controller('CoolController', ['$scope',
+    function($scope) {
+
+      $scope.allTasks = [];
+
+      var Todo = function(taskItem) {
+        this.task = taskItem;
+        this.complete = false;
+      };
+
+      $scope.remaining = function() {
+        var remainingItems = _.filter($scope.allTasks, function(t) {
+          return t.complete === false;
+        });
+        return remainingItems.length;
+      };
+
+      $scope.addTodo = function(x) {
+        var t = new Todo(x.item);
+        $scope.allTasks.push(t);
+        $scope.task = {};
+      };
+
+      $scope.toggleStatus = function(y) {
+        y.complete = (y.complete) ? false : true;
+      };
+
+    }
+
+  ]);
+
+}());
